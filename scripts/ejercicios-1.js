e87ad134a4446d394fb895c30ab3759646d80a9f
Adan <adanclintonvizcarrychoque@gmail.com>
2025-11-26 21:58:54 -0500

    probando el teclado

--- scripts/ejercicios-1.js
+++ scripts/ejercicios-1.js
@@ -168,5 +168,12 @@ function miFunct8(palabra, patron) {
     }
 }
 miFunct8("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
+//9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+function miFunct9(){
+    return Math.random()*500
+}
 /*
+10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 */
